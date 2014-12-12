256c569413c1a6f91ca94fa08135035fbb4b7370
ChihMin <s102062111@m102.nthu.edu.tw>
2014-12-12 21:24:25 +0800

    init and add ajax detect

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+	require 'rest_client'
+	require 'time'
+	require 'json'
+
+	def index
+		@jsonString = 
+		RestClient.get 'http://106.186.30.234/api/devices/c6a89825f1364bed/datastreams', 
+		{
+			:params => 
+			{
+				:apikey => 'c3ec40a829b5424c', 
+				:device_id => 'c6a89825f1364bed',
+			} ,
+			:accept => :json
+		} 
+		@res = JSON.parse( @jsonString )
+	end
+	def get_data
+		@jsonString = 
+		RestClient.get 'http://106.186.30.234/api/devices/c6a89825f1364bed/datastreams', 
+		{
+			:params => 
+			{
+				:apikey => 'c3ec40a829b5424c', 
+				:device_id => 'c6a89825f1364bed',
+			} ,
+			:accept => :json
+		} 
+		@res = JSON.parse( @jsonString )
+	end
+end
