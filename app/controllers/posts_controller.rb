e330bf7a75f5c4c8eade57d532f40e014c469ede
ChihMin <s102062111@m102.nthu.edu.tw>
2014-12-13 18:44:59 +0800

    add faq and refresh position of png

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,17 +20,7 @@ class PostsController < ApplicationController
 	      format.json { render :json => @jsonString }
 	    end
 	end
-	def get_data
-		@jsonString = 
-		RestClient.get 'http://106.186.30.234/api/devices/c6a89825f1364bed/datastreams', 
-		{
-			:params => 
-			{
-				:apikey => 'c3ec40a829b5424c', 
-				:device_id => 'c6a89825f1364bed',
-			} ,
-			:accept => :json
-		} 
-		
+
+	def faq
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index'
+  get 'posts/faq'
   get 'posts/get_data'
   root 'posts#index'
 
